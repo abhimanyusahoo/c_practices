093c08cb4c80570a680669f4eb2438fc401c5999
Abhimanyu Sahoo <161146329+abhimanyusahoo@users.noreply.github.com>
2024-09-20 15:26:45 +0530

    Add files via upload

---
+++ decimaltobinary.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int dtb(int m){
+    int binarynum[32];
+    int i=0;
+    
+    while(m>0){
+        binarynum[i]=m%2;
+        m=m/2;
+        i++;
+    }
+	int k;
+	
+    for(k=i-1;k>=0;k--){
+        printf("%d",binarynum[k]);
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+
+    dtb(a);
+
+    return 0;
+}
